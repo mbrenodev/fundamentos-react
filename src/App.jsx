b46f58dcde523688e5f115584618196ed9d9f0b5
mbreno-lenovo <mbreno.dev@gmail.com>
2023-08-02 19:58:41 -0300

    components-props

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {Post} from './Post'
+
+export function App() {
+  return (
+    <>
+      <Post 
+          author="Fernanda Cardoso" 
+          content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum architecto facilis mollitia dignissimos corrupti nemo aperiam veritatis. Aperiam magnam sequi obcaecati blanditiis, exercitationem eligendi corrupti repellat numquam natus accusamus nobis." 
+      />
+    </>
+  );
+}
+
+
