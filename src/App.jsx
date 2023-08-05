316481cdf11ad6819bd4d9efe8a63186a66d731e
mbreno-lenovo <mbreno.dev@gmail.com>
2023-08-05 08:51:57 -0300

    add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import {Post} from './Post'
 import { Header }  from './components/Header'
+import { Sidebar } from './components/Sidebar';
+
+import styles from "./App.module.css"
 import './global.css'
 
 
@@ -7,10 +10,16 @@ export function App() {
   return (
     <>
       <Header />
-      <Post 
-          author="Fernanda Cardoso" 
-          content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum architecto facilis mollitia dignissimos corrupti nemo aperiam veritatis. Aperiam magnam sequi obcaecati blanditiis, exercitationem eligendi corrupti repellat numquam natus accusamus nobis." 
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+              author="Fernanda Cardoso" 
+              content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum architecto facilis mollitia dignissimos corrupti nemo aperiam veritatis. Aperiam magnam sequi obcaecati blanditiis, exercitationem eligendi corrupti repellat numquam natus accusamus nobis." 
+          />
+        </main>
+      </div>
     </>
   );
 }
